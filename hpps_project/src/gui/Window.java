6f7d212eb090a0f8c3a4e84d7b96b15d89f7df85
alessandro.tribi@gmail.com <alessandro.tribi@gmail.com@d6301ede-72e2-85b2-efb7-2d4d06ce838e>
2014-07-09 14:43:37 +0000

    Window modified

--- hpps_project/src/gui/Window.java
+++ hpps_project/src/gui/Window.java
@@ -3,8 +3,10 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -16,7 +18,16 @@ public class Window extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JSlider slider1;
+	private JSlider slider2;
+	private JSlider slider3;
+	private JSlider slider4;
+	private JSlider slider5;
+	private JSlider slider6;
+	private JSlider slider7;
+	private JSlider slider8;
 	private JTextField textField;
+	private JButton jButton;
+	private JScrollPane scrollPane;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -41,12 +52,37 @@ public class Window extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		slider1 = new JSlider();
-		textField = new JTextField();
-		contentPane.add(slider1);
-		contentPane.add(textField);
-		getContentPane().add(contentPane, BorderLayout.CENTER);
-		textField.setColumns(10);
 		slider1.setOrientation(SwingConstants.VERTICAL);
+		slider2 = new JSlider();
+		slider2.setOrientation(SwingConstants.VERTICAL);
+		slider3 = new JSlider();
+		slider3.setOrientation(SwingConstants.VERTICAL);
+		slider4 = new JSlider();
+		slider4.setOrientation(SwingConstants.VERTICAL);
+		slider5 = new JSlider();
+		slider5.setOrientation(SwingConstants.VERTICAL);
+		slider6 = new JSlider();
+		slider6.setOrientation(SwingConstants.VERTICAL);
+		slider7 = new JSlider();
+		slider7.setOrientation(SwingConstants.VERTICAL);
+		slider8 = new JSlider();
+		slider8.setOrientation(SwingConstants.VERTICAL);
+		textField = new JTextField();
+		textField.setColumns(10);
+		jButton = new JButton();
+		scrollPane = new JScrollPane();
+		contentPane.add(slider1);
+		contentPane.add(slider2);
+		contentPane.add(slider3);
+		contentPane.add(slider4);
+		contentPane.add(slider5);
+		contentPane.add(slider6);
+		contentPane.add(slider7);
+		contentPane.add(slider8);
+		contentPane.add(textField);
+		contentPane.add(jButton);
+		contentPane.add(scrollPane);
+		getContentPane().add(contentPane, BorderLayout.CENTER);
 	}
 	
 	private void actions() {
