1bf30d76b177d96bc2e6588dfbf5d67c2b44849d
alessandro.tribi@gmail.com <alessandro.tribi@gmail.com@d6301ede-72e2-85b2-efb7-2d4d06ce838e>
2014-07-09 13:54:18 +0000

    First Gui+Logic

--- hpps_project/src/gui/Window.java
+++ hpps_project/src/gui/Window.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,17 +11,32 @@ import javax.swing.event.ChangeListener;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 
-public class ClasseW extends JFrame {
+public class Window extends JFrame {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JSlider slider1;
 	private JTextField textField;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window frame = new Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-	public ClasseW() {
+	public Window() {
+		initialize();
+		actions();
+	}
+	
+	private void initialize() {
 		setBounds(100, 100, 450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
@@ -31,16 +47,13 @@ public class ClasseW extends JFrame {
 		getContentPane().add(contentPane, BorderLayout.CENTER);
 		textField.setColumns(10);
 		slider1.setOrientation(SwingConstants.VERTICAL);
-
+	}
+	
+	private void actions() {
 		slider1.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				textField.setText("Time: " + slider1.getValue());
-				System.out.println("Slider1: " + slider1.getValue());
 			}
 		});
-	}
-
-	public static void main(String[] args) {
-		new ClasseW().setVisible(true);
 	}
 }
---
+++ hpps_project/src/logic/StartLogic.java
@@ -0,0 +1,11 @@
+package logic;
+
+import gui.Window;
+
+public class StartLogic {
+	
+	public static void main(String[] args) {
+		Window startWindow = new Window();
+		startWindow.setVisible(true);
+	}
+}
