5bffb30cfce959b7edb97aee64b65e780e88c3aa
manuel.grillo@aol.it <manuel.grillo@aol.it@d6301ede-72e2-85b2-efb7-2d4d06ce838e>
2014-07-10 12:19:13 +0000

    Altra sistemata. *Credo* che bisogna cambiare il listener del bottone (e.g. Action anziché change) (Tentativo 30495758935689 di commit.)

--- hpps_project/src/gui/TestOne.java
+++ hpps_project/src/gui/TestOne.java
@@ -1,40 +1,23 @@
 package gui;
 
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
-import java.util.ArrayList;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class TestOne extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JFrame frame;
-	private JScrollPane scrollPane;
 	private JTextArea textArea;
+	private String s="not initialized";
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TestOne window = new TestOne();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public TestOne() {
+	public TestOne(String s){
+		this.s=s;
 		initialize();
+		this.frame.setVisible(true);
 	}
 
 	/**
@@ -45,17 +28,10 @@ public class TestOne extends JFrame {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
 		textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
-	}
-	
-	public void setTestOne(ArrayList<String> data) {
-		int i;
-		for(i=0; i<data.size(); i++)
-		textArea.append(data.get(i));
+		textArea.setEditable(false);
+		frame.getContentPane().add(textArea, BorderLayout.CENTER);
+		textArea.append(s);
 	}
 
 }
--- hpps_project/src/logic/DataManager.java
+++ hpps_project/src/logic/DataManager.java
@@ -6,15 +6,10 @@ import gui.TestOne;
 
 public class DataManager {
 	
-	public DataManager(ArrayList<String> a) {
-		this.data = a;	
-	}
-	
-	private ArrayList<String> data;
+	//private ArrayList<String> data;
 	
 	public static void putInTestOne(ArrayList<String> data){
-		TestOne startTestOne = new TestOne();
-		startTestOne.setTestOne(data);
+		TestOne startTestOne = new TestOne(data.toString());
 		startTestOne.setVisible(true);
 	}
 }
